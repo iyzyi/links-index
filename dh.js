c29ab65a48718bd1cd8a966878e2286d39664366
iyzyi <kljxn@qq.com>
2020-02-20 10:38:00 +0800

    改成自己的网址（goto页面404？？）

--- dh.js
+++ dh.js
@@ -1,492 +1,56 @@
-var dh_data = [{
-    "name":"搜索 · 网页",
-    "item": [
-        {
-            "name": "百度",
-            "url": "https://www.baidu.com/"
-        },
-        {
-            "name": "谷歌",
-            "url": "https://www.google.com/"
-        },
-        {
-            "name": "必应",
-            "url": "https://www.bing.com/"
-        },        
-        {
-            "name": "Magi",
-            "url": "https://magi.com/"
-        },
-        {
-            "name": "秘迹搜索",
-            "url": "https://mijisou.com/"
-        },
-        {
-            "name": "神马",
-            "url": "https://m.sm.cn/"
-        },
-        {
-            "name": "搜狗",
-            "url": "https://www.sogou.com/"
-        },
-        {
-            "name": "多吉搜索",
-            "url": "https://www.dogedoge.com/"
-        },
-        {
-            "name": "Oscobo",
-            "url": "https://www.oscobo.com/"
-        }
-    ]
-},
-{
-    "name":"搜索 · 磁力",
-    "item": [
-        {
-            "name": "BTSOW",
-            "url": "https://tellme.pw/btsow"
-        },
-        {
-            "name": "SOBT",
-            "url": "http://www.sobt5.pw/"
-        },
-        {
-            "name": "磁力狗",
-            "url": "http://ciligou.top/"
-        },
-        {
-            "name": "磁力蜘蛛",
-            "url": "http://www.eclzz.xyz/"
-        },
-        {
-            "name": "老王磁力",
-            "url": "https://url302.icu/1dV1t25j49"
-        },
-        {
-            "name": "磁力猪",
-            "url": "https://www.clilizz.com/"
-        },
-        {
-            "name": "磁力狗",
-            "url": "https://dh.dogsdxz.com/"
-        },        
-        {
-            "name": "BtHaha",
-            "url": "https://wangzhi.men/bthaha/"
-        },
-        {
-            "name": "磁力车",
-            "url": "http://5i0.fun/6qPP"
-        }
-    ]
-},
-{
-    "name":"搜索 · 图片",
-    "item": [
-        {
-            "name": "百度识图",
-            "url": "https://image.baidu.com"
-        },
-        {
-            "name": "Google识图",
-            "url": "https://images.google.com/"
-        },
-        {
-            "name": "TinEye",
-            "url": "https://www.tineye.com/"
-        },        
-        {
-            "name": "Yandex识图",
-            "url": "https://www.yandex.com/images/"
-        },
-        {
-            "name": "360识图",
-            "url": "https://st.so.com/"
-        },
-        {
-            "name": "搜狗识图",
-            "url": "https://pic.sogou.com"
-        },
-        {
-            "name": "SauceNAO",
-            "url": "https://saucenao.com/"
-        },
-        {
-            "name": "IQDB",
-            "url": "http://www.iqdb.org/"
-        },
-        {
-            "name": "二箱",
-            "url": "https://ainoob.com/zh/project/noobox"
-        }
-    ]
-},
-{
-    "name":"美图 · 在线",
-    "item": [
-        {
-            "name": "美图录",
-            "url": "https://www.meitulu.com/"
-        },
-        {
-            "name": "美女图",
-            "url": "http://www.mtmnt.com/"
-        },
-        {
-            "name": "妹子图",
-            "url": "https://www.mzitu.com/"
-        },        
-        {
-            "name": "爱美女",
-            "url": "https://www.2meinv.com/"
-        },
-        {
-            "name": "妹子天空",
-            "url": "http://www.girlsky.cn/mntp/"
-        },
-        {
-            "name": "MM131",
-            "url": "https://www.mm131.net/xinggan/"
-        },
-        {
-            "name": "绝对服从",
-            "url": "http://www.jueduifucong.com/"
-        },
-        {
-            "name": "天天套图",
-            "url": "http://www.tttt8.net/"
-        },
-        {
-            "name": "图虫性感",
-            "url": "https://tuchong.com/tags/%E6%80%A7%E6%84%9F"
-        }
-    ]
-},
-{
-    "name":"动漫 · 相关",
-    "item": [
-        {
-            "name": "番剧放送",
-            "url": "https://bgmlist.com/"
-        },
-        {
-            "name": "Neets",
-            "url": "https://neets.cc/category?type=animation"
-        },
-        {
-            "name": "動漫花園",
-            "url": "http://share.dmhy.org/"
-        },        
-        {
-            "name": "爱恋动漫",
-            "url": "http://www.kisssub.org/"
-        },
-        {
-            "name": "MioBT",
-            "url": "http://www.miobt.com/"
-        },
-        {
-            "name": "萌娘百科",
-            "url": "https://zh.moegirl.org"
-        },
-        {
-            "name": "仓库",
-            "url": "https://cangku.moe"
-        },
-        {
-            "name": "琉璃",
-            "url": "http://www.liuli.in"
-        },
-        {
-            "name": "熊猫书签",
-            "url": "https://panda.52linglong.com/"
-        }
-    ]
-},
-{
-    "name":"软件 · 绿软",
-    "item": [
-        {
-            "name": "吾爱破解",
-            "url": "https://www.52pojie.cn"
-        },
-        {
-            "name": "zd423",
-            "url": "http://www.zdfans.com/"
-        },
-        {
-            "name": "大眼仔",
-            "url": "http://www.dayanzai.me/"
-        },        
-        {
-            "name": "殁漂遥",
-            "url": "https://www.mpyit.com/"
-        },
-        {
-            "name": "胡萝卜周",
-            "url": "http://www.carrotchou.blog/"
-        },
-        {
-            "name": "小众软件",
-            "url": "https://www.appinn.com/"
-        },
-        {
-            "name": "精品绿软",
-            "url": "https://www.portablesoft.org/"
-        },
-        {
-            "name": "软件缘",
-            "url": "https://www.appcgn.com/"
-        },
-        {
-            "name": "V5PC",
-            "url": "http://www.v5pc.com/"
-        }
-    ]
-},
-{
-    "name":"电影 · 在线",
-    "item": [
-        {
-            "name": "看看屋",
-            "url": "https://www.kankanwu.com/"
-        },
-        {
-            "name": "980影院",
-            "url": "http://www.980kp.com"
-        },
-        {
-            "name": "VIP电影站",
-            "url": "http://vipdyz.cn/"
-        },        
-        {
-            "name": "完美看看",
-            "url": "https://www.wanmeikk.me/"
-        },
-        {
-            "name": "1090影视",
-            "url": "https://1090ys.com/"
-        },
-        {
-            "name": "豌豆影视",
-            "url": "https://www.wandouys.com/"
-        },
-        {
-            "name": "6V电影",
-            "url": "https://www.66s.cc"
-        },
-        {
-            "name": "急速影视",
-            "url": "https://www.jisuysw.com/"
-        },
-        {
-            "name": "海狸影视",
-            "url": "http://www.hailiys.com/"
-        }
-    ]
-},
-{
-    "name":"电影 · 下载",
-    "item": [
-        {
-            "name": "MP4ba",
-            "url": "http://www.mp4ba.cc/"
-        },
-        {
-            "name": "优质电影网",
-            "url": "http://www.youzhidy.com/"
-        },
-        {
-            "name": "BT首发",
-            "url": "http://www.btshoufa.net"
-        },        
-        {
-            "name": "中国高清网",
-            "url": "http://gaoqing.la/"
-        },
-        {
-            "name": "音范丝",
-            "url": "https://www.yinfans.me/"
-        },
-        {
-            "name": "BT之家",
-            "url": "http://www.btbtt.co/"
-        },
-        {
-            "name": "片源网",
-            "url": "http://pianyuan.net/"
-        },
-        {
-            "name": "PianHD",
-            "url": "http://www.pianhd.com/"
-        },
-        {
-            "name": "片库",
-            "url": "https://www.pianku.tv/"
-        }
-    ]
-},
-{
-    "name":"电影 · APP",
-    "item": [
-        {
-            "name": "大鱼视频",
-            "url": "http://tv.fish/"
-        },
-        {
-            "name": "新影视大全",
-            "url": "http://mfba.cc/382.html"
-        },
-        {
-            "name": "大象视频",
-            "url": "http://www.dx99.me/"
-        },        
-        {
-            "name": "福吧影视",
-            "url": "http://www.wnflb66.com/fulibamovie_lastest.apk"
-        },
-        {
-            "name": "韩剧TV",
-            "url": "http://hanju.koudaibaobao.com/"
-        },
-        {
-            "name": "人人影视",
-            "url": "http://www.yyets.com/"
-        },
-        {
-            "name": "磁力TV",
-            "url": "http://magnetv.cn/download/"
-        },
-        {
-            "name": "待添加",
-            "url": "/"
-        },
-        {
-            "name": "待添加",
-            "url": "/"
-        }
-    ]
-},
-{
-    "name":"网盘 · 长期",
-    "item": [
-        {
-            "name": "百度网盘",
-            "url": "https://pan.baidu.com"
-        },
-        {
-            "name": "OneDrive",
-            "url": "https://onedrive.live.com/about/zh-cn/"
-        },
-        {
-            "name": "腾讯微云",
-            "url": "https://www.weiyun.com/"
-        },        
-        {
-            "name": "天翼云盘",
-            "url": "https://cloud.189.cn/"
-        },
-        {
-            "name": "115网盘",
-            "url": "https://115.com/"
-        },
-        {
-            "name": "蓝奏云",
-            "url": "https://www.lanzou.com/"
-        },
-        {
-            "name": "比特球",
-            "url": "https://pan.bitqiu.com/invite-friend?mafrom=acquisition&mipos=invitee&uid=105016766"
-        },
-        {
-            "name": "Google Drive",
-            "url": "https://drive.google.com"
-        },
-        {
-            "name": "Dropbox",
-            "url": "https://www.dropbox.com/"
-        }
-    ]
-},
-{
-    "name":"网盘 · 临时",
-    "item": [
-        {
-            "name": "奶牛快传",
-            "url": "https://cowtransfer.com/"
-        },
-        {
-            "name": "Send Firefox",
-            "url": "https://send.firefox.com/"
-        },
-        {
-            "name": "BitSend",
-            "url": "https://bitsend.jp/?setLang=zh-tw"
-        },        
-        {
-            "name": "TMP link",
-            "url": "http://tmp.link/"
-        },
-        {
-            "name": "Tempfile",
-            "url": "https://tempfile.cloud/"
-        },
-        {
-            "name": "Airportal",
-            "url": "https://ap.rthe.cn"
-        },
-        {
-            "name": "Filemail",
-            "url": "https://www.filemail.com/"
-        },
-        {
-            "name": "Sendit",
-            "url": "https://sendit.cloud/"
-        },
-        {
-            "name": "Fileio",
-            "url": "https://www.file.io/"
-        }
-    ]
-},
-{
-    "name":"网盘 · 搜索",
-    "item": [
-        {
-            "name": "云盘精灵",
-            "url": "https://www.yunpanjingling.com/"
-        },
-        {
-            "name": "猪猪盘",
-            "url": "http://www.zhuzhupan.com/"
-        },
-        {
-            "name": "盘多多",
-            "url": "http://www.panduoduo.net/"
-        },        
-        {
-            "name": "大力盘",
-            "url": "https://www.dalipan.com/"
-        },
-        {
-            "name": "小可搜索",
-            "url": "https://www.xiaokesoso.com/"
-        },
-        {
-            "name": "盘搜",
-            "url": "http://www.pansou.com/"
-        },
-        {
-            "name": "小白盘",
-            "url": "http://www.xiaobaipan.com/"
-        },
-        {
-            "name": "众人搜索",
-            "url": "http://wangpan.renrensousuo.com/"
-        },
-        {
-            "name": "云盘恶魔",
-            "url": "https://pan.09l.me/"
-        }
-    ]
-}]
+var dh_data = [
+    {
+        "name":"基地",
+        "item": [
+            {
+                "name": "开始界面",
+                "url": "http://iyzyi.com"
+            },
+            {
+                "name": "博客主站",
+                "url": "http://blog.iyzyi.com"
+            },
+            {
+                "name": "hexo博客",
+                "url": "http://hexo.iyzyi.com"
+            },        
+            {
+                "name": "Github",
+                "url": "https://github.com/"
+            },
+            {
+                "name": "Docker Hub",
+                "url": "https://hub.docker.com/u/iyzyi"
+            }
+        ]
+    },
+    {
+        "name":"服务",
+        "item": [
+            {
+                "name": "宝塔-1",
+                "url": "http://bt.iyzyi.com"
+            },
+            {
+                "name": "宝塔-2",
+                "url": "http://bt.iyzyi.cn"
+            },
+            {
+                "name": "下载-1",
+                "url": "http://aria.iyzyi.com"
+            },
+            {
+                "name": "下载-2",
+                "url": "http://aria.iyzyi.cn"
+            },
+            {
+                "name": "文件-1",
+                "url": "http://file.iyzyi.com"
+            },
+            {
+                "name": "文件-2",
+                "url": "http://file.iyzyi.cn"
+            }
+        ]
+    }
+]
